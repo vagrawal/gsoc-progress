aaa8123c67de8afad752c1d69c4687b52c4b6a96
Muhammad Ahmed Shah <mshah1@deepnet0.qatar.cmu.local>
2017-05-27 07:53:41 -0400

    mlp

--- mlpTest.py
+++ mlpTest.py
@@ -1,18 +1,15 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.utils import to_categorical
-import resnet
 import numpy as np
 
 data = np.load('wsj0_randlabels.npz')
-# model = Sequential()
-# model.add(Dense(1000, activation='sigmoid', input_dim=40))
-# model.add(Dense(1000, activation='sigmoid'))
-# model.add(Dense(1000, activation='sigmoid'))
-# model.add(Dense(1000, activation='sigmoid'))
-# model.add(Dense(3000,activation='softmax'))
-builder = resnet.ResnetBuilder()
-model = builder.build_resnet_152((1,1,40),3000)
+model = Sequential()
+model.add(Dense(1000, activation='sigmoid', input_dim=40))
+model.add(Dense(1000, activation='sigmoid'))
+model.add(Dense(1000, activation='sigmoid'))
+model.add(Dense(1000, activation='sigmoid'))
+model.add(Dense(3000,activation='softmax'))
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
