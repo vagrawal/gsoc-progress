80cfdbdda0a590964b0af827da53a69b2bd97c20
Muhammad Ahmed Shah <mshah1@deepnet0.qatar.cmu.local>
2017-06-08 06:32:04 -0400

    notebook update

--- shinigami1494/mlpTest.py
+++ shinigami1494/mlpTest.py
@@ -1,8 +1,8 @@
 from keras.models import Sequential
 from keras.optimizers import SGD
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Input, Dense, Activation, Dropout, add
 from keras.utils import to_categorical, plot_model
-from keras.models import load_model
+from keras.models import load_model, Model
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -10,25 +10,26 @@ import tensorflow as tf
 # print mpl.matplotlib_fname()
 # mpl.rcParams['backend'] = 'agg'
 # plt = mpl.pyplot
-def mlp1():
+def mlp1(input_dim,output_dim,depth):
 	model = Sequential()
-	model.add(Dense(1000, activation='relu', input_dim=40))
-	model.add(Dense(1000, activation='relu'))
-	model.add(Dense(3000, activation='softmax'))
+	model.add(Dense(1000, activation='relu', input_dim=input_dim))
+	for i in range(depth):
+		model.add(Dense(1000, activation='relu'))
+	model.add(Dense(output_dim, activation='softmax'))
 	model.compile(optimizer='adagrad',
 	              loss='categorical_crossentropy',
 	              metrics=['accuracy'])
 	return model
 
-def mlp2():
+def mlp2(input_dim,output_dim):
 	model = Sequential()
-	model.add(Dense(1000, activation='relu', input_dim=40))
+	model.add(Dense(1000, activation='relu', input_dim=input_dim))
 	model.add(Dropout(0.5))
 	model.add(Dense(1000, activation='relu'))
 	model.add(Dropout(0.5))
 	model.add(Dense(1000, activation='relu'))
 	model.add(Dropout(0.5))
-	model.add(Dense(3000, activation='softmax'))
+	model.add(Dense(output_dim, activation='softmax'))
 	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
 	model.compile(loss='categorical_crossentropy',
@@ -36,9 +37,9 @@ def mlp2():
 	              metrics=['accuracy'])
 	return model
 
-def mlp3():
+def mlp3(input_dim,output_dim):
 	model = Sequential()
-	model.add(Dense(500, activation='relu', input_dim=40))
+	model.add(Dense(500, activation='relu', input_dim=input_dim))
 	model.add(Dropout(0.25))
 	model.add(Dense(500, activation='relu'))
 	model.add(Dropout(0.25))
@@ -46,31 +47,67 @@ def mlp3():
 	model.add(Dropout(0.25))
 	model.add(Dense(500, activation='relu'))
 	model.add(Dropout(0.25))
-	model.add(Dense(3000, activation='softmax'))
+	model.add(Dense(output_dim, activation='softmax'))
 	model.compile(optimizer='adagrad',
 	              loss='categorical_crossentropy',
 	              metrics=['accuracy'])
 	return model
-data = np.load('wsj0_randlabels.npz')
-model = mlp1()
-plot_model(model, to_file='mlp3_model.png')
-# x_train = data['X_Train'][:1000000]
-# y_train = data['Y_Train'][:1000000]
-# y_train = to_categorical(y_train, num_classes = 3000)
-# history = model.fit(x_train,y_train,epochs=10,batch_size=20000)
-# print(history.history.keys())
-# # summarize history for accuracy
-# plt.plot(history.history['acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.savefig('mlp1_relu_acc.png')
+
+def make_dense_res_block(inp, size):
+	x = inp
+	for i in range(size):
+		x = Dense(500, activation='relu')(x)
+		x = Dropout(0.25)(x)
+	return x
+
+def mlp4(input_dim,output_dim,nBlocks):
+	inp = Input(shape=(input_dim,))
+	x = Dense(500, activation='relu')(inp)
+	for i in range(nBlocks):
+		y = make_dense_res_block(x,2)
+		x = add([x,y])
+	z = Dense(output_dim, activation='softmax')(x)
+	model = Model(inputs=inp, outputs=z)
+	model.compile(optimizer='rmsprop',
+	              loss='categorical_crossentropy',
+	              metrics=['accuracy'])
+	return model
+
+
+
+# model = mlp4(20, 132,10)
+# plot_model(model, to_file='mlp4_model.png')
+print 'loading data...'
+data = np.load('wsj0_phonelabels.npz')
+x_train = data['X_Train']
+y_train = data['Y_Train']
+print 'transforming labels...'
+nClasses = int(np.max(y_train) + 1)
+y_train = to_categorical(y_train, num_classes = nClasses)
+
+print 'initializing model...'
+model = mlp4(x_train.shape[1], nClasses, 10)
+print 'starting fit...'
+history = model.fit(x_train,y_train,epochs=10,batch_size=20000)
+model.save('mlp4_phonelabels.h5')
+print(history.history.keys())
+
+print 'plotting graphs...'
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.savefig('mlp4_phonedata_relu_rmsprop.png')
 
 
 # model = load_model("mlp_randomAlign.h5")
-# x_test = data['X_Test'][:250000]
-# y_test = data['Y_Test'][:250000]
-# y_test = to_categorical(y_test, num_classes = 3000)
-# with tf.device('/gpu:0'):
-# 	score = model.evaluate(x_test, y_test, batch_size=2000)
-# print score
+print 'loading test data...'
+x_test = data['X_Test']
+y_test = data['Y_Test']
+y_train = data['Y_Train'].astype(int)
+print 'transforming labels...'
+y_test = to_categorical(y_test, num_classes = np.max(y_train) + 1)
+print 'scoring...'
+score = model.evaluate(x_test, y_test, batch_size=2000)
+print score
